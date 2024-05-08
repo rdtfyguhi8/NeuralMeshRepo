39b279e3c0cae9373c418dab2ee07e0686fbdfca
rdtfyguhi8 <js1otocraft@hotmail.com>
2024-05-09 04:02:08 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there are elements remaining
+  while (currentIndex !== 0) {
+    // Pick a remaining element
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // Swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
+
+const shuffledDeck = shuffle([1, 2, 3, 4, 5]);
+console.log(shuffledDeck);
