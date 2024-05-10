0bc84bbdd3723c5750f14e027725ed916e16900f
rdtfyguhi8 <js1otocraft@hotmail.com>
2024-05-11 03:30:27 +0700

    update sdk task commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hi world");
+function subarraySum(nums, k) {
+  const map = new Map();
+  map.set(0, 1);
+  let count = 0;
+  let sum = 0;
+  for (const num of nums) {
+    sum += num;
+    if (map.has(sum - k)) {
+      count += map.get(sum - k);
+    }
+    map.set(sum, (map.get(sum) || 0) + 1);
+  }
+  return count;
+}
